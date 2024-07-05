89f49ed97c9da73493e2f5ae82eea1ae069270ed
Stefan Arsic <stefanarsic1201@gmail.com>
2024-07-05 17:18:17 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const navLinks= document.getElementById('links');
+const btn= document.getElementById('btn');
+btn.addEventListener('click',()=>{
+    navLinks.classList.toggle('active')
+});
+    
+
